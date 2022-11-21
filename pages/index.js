e2e62c6d2b660bfe274d80f3e9d0fa4fcbb417c4
bezlant <abezludniy@mail.ru>
2022-11-21 13:53:01 +0300

    feat: popup works now

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,33 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { useState, useEffect } from "react";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
 
-import abi from "../abi.json";
+import abi from '../abi.json'
 
 export default function Home() {
-  const [web3, setWeb3] = useState(null);
-  const [walletAddress, setWalletAddress] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [connected, setConnected] = useState(false);
+  const [web3, setWeb3] = useState(null)
+  const [walletAddress, setWalletAddress] = useState(null)
+  const [contract, setContract] = useState(null)
+  const [connected, setConnected] = useState(false)
 
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState(0)
 
-  const [team, setTeam] = useState("");
-  const [isTeamChosen, setIsTeamChosen] = useState(false);
-  const [choseTeamErr, setChoseTeamErr] = useState(false);
+  const [team, setTeam] = useState('')
+  const [isTeamChosen, setIsTeamChosen] = useState(false)
+  const [choseTeamErr, setChoseTeamErr] = useState(false)
 
-  const [hasJoined, setHasJoined] = useState(false);
+  const [hasJoined, setHasJoined] = useState(false)
 
-  const [showPotentialGain, setShowPotentialGain] = useState(false);
-  const [potentialGain, setPotentialGain] = useState(0);
+  const [showPotentialGain, setShowPotentialGain] = useState(false)
+  const [potentialGain, setPotentialGain] = useState(0)
 
-  const contractAddress = "0xF9772ca577617c86ef33A5E4725dA4B960190787";
-  const gasLimit = 285000;
-  const Web3 = require("web3");
+  const [bid, setBid] = useState({ team: 0, address: 0, id: 0 })
+  const [hasEventFired, setHasEventFired] = useState(false)
+
+  const contractAddress = '0xF9772ca577617c86ef33A5E4725dA4B960190787'
+  const gasLimit = 285000
+  const Web3 = require('web3')
 
   // We do this when our page is done loading in a useEffect hook and put it into our state for later use.
   useEffect(() => {
@@ -32,61 +35,85 @@ export default function Home() {
       // MetaMask injects the window.ethereum object into our browser whenever the extension is installed and active
       if (window.ethereum) {
         ethereum
-          .request({ method: "eth_requestAccounts" })
+          .request({ method: 'eth_requestAccounts' })
           .then((accounts) => {
             // If the object has been found, we set the Web3 object in our React state and also the logged-in address from MetaMask that we need to use later on.
-            setWalletAddress(accounts[0]);
-            let w3 = new Web3(ethereum);
-            setWeb3(w3);
+            setWalletAddress(accounts[0])
+            let w3 = new Web3(ethereum)
+            setWeb3(w3)
             // Adding the ABI to our smart contract in the code
-            let contract = new w3.eth.Contract(abi, contractAddress);
-            setContract(contract);
-            setConnected(true);
+            let contract = new w3.eth.Contract(abi, contractAddress)
+            setContract(contract)
+            setConnected(true)
           })
-          .catch((err) => console.log(err));
+          .catch((err) => console.log(err))
       } else {
-        console.log("Please install MetaMask");
+        console.log('Please install MetaMask')
       }
     } else {
+      eventPopUp()
       contract.methods
         .Address_Amount(String(walletAddress))
         .call()
         .then((currentBidAmount) => {
           // User has placed a bid on the blockchain
           if (Number(currentBidAmount) > 0) {
-            setHasJoined(true);
-            setAmount(currentBidAmount / 1000000000000000000);
+            setHasJoined(true)
+            setAmount(currentBidAmount / 1000000000000000000)
           }
 
           isTeamChosen
             ? setShowPotentialGain(true)
-            : setShowPotentialGain(false);
-          calculatePotentialGain();
-        });
+            : setShowPotentialGain(false)
+          calculatePotentialGain()
+        })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [amount, connected, team]);
+  }, [amount, connected, team])
+
+  const eventPopUp = () => {
+    console.log('event popup')
+    const placedEvent = contract.events.Placed(
+      {
+        filter: { ADDRESS: walletAddress },
+      },
+      (err, e) => {
+        if (err) {
+          console.log(err)
+          return
+        } else {
+          const res = e.returnValues
+          setBid({
+            team: res.OPTION,
+            address: res.ADDRESS,
+            id: res.ID,
+          })
+          setHasEventFired(true)
+        }
+      }
+    )
+  }
 
   const calculatePotentialGain = () => {
     contract.methods
       .GetOptionPool()
       .call()
       .then((pool) => {
-        const leftTeamPool = Number(pool[0]);
-        const rightTeamPool = Number(pool[1]);
-        const userPool = Number(team === "left" ? leftTeamPool : rightTeamPool);
+        const leftTeamPool = Number(pool[0])
+        const rightTeamPool = Number(pool[1])
+        const userPool = Number(team === 'left' ? leftTeamPool : rightTeamPool)
         const totalGain =
-          (amount / (leftTeamPool + rightTeamPool)) * (userPool + amount) * 0.9;
-        setPotentialGain(totalGain.toFixed(6));
-      });
-  };
+          (amount / (leftTeamPool + rightTeamPool)) * (userPool + amount) * 0.9
+        setPotentialGain(totalGain.toFixed(6))
+      })
+  }
 
   const processBid = () => {
     // Check if team is chosen
-    if (team === "") {
-      setChoseTeamErr(true);
-      setTimeout(() => setChoseTeamErr(false), 2500);
-      return;
+    if (team === '') {
+      setChoseTeamErr(true)
+      setTimeout(() => setChoseTeamErr(false), 2500)
+      return
     }
 
     // Check if the user has already joined the bidding
@@ -95,17 +122,17 @@ export default function Home() {
       .call()
       .then((currentBidAmount) => {
         if (Number(currentBidAmount) > 0) {
-          setHasJoined(true);
+          setHasJoined(true)
         } else {
-          sendBidToBlockchain();
+          sendBidToBlockchain()
         }
-      });
-  };
+      })
+  }
 
   const sendBidToBlockchain = () => {
-    const sendAmount = amount * 1000000000000000000;
+    const sendAmount = amount * 1000000000000000000
     switch (team) {
-      case "left":
+      case 'left':
         contract.methods
           .placeat0()
           .send({
@@ -115,10 +142,10 @@ export default function Home() {
             value: String(sendAmount),
           })
           .then(() => {
-            console.log("Success");
-          });
-        break;
-      case "right":
+            console.log('Success')
+          })
+        break
+      case 'right':
         contract.methods
           .placeat1()
           .send({
@@ -128,13 +155,13 @@ export default function Home() {
             value: String(sendAmount),
           })
           .then(() => {
-            console.log("Success!");
-          });
-        break;
+            console.log('Success!')
+          })
+        break
       default:
-        console.log(`Error! The ${team} doesn't exist`);
+        console.log(`Error! The ${team} doesn't exist`)
     }
-  };
+  }
 
   return (
     <div className={styles.container}>
@@ -153,20 +180,20 @@ export default function Home() {
         <div className={styles.grid}>
           <div
             onClick={() => {
-              setTeam("left");
-              setIsTeamChosen(true);
+              setTeam('left')
+              setIsTeamChosen(true)
             }}
-            className={team === "left" ? styles.cardClicked : styles.card}
+            className={team === 'left' ? styles.cardClicked : styles.card}
           >
             <h2>Left</h2>
           </div>
           <div className={styles.bidInput}>
             <label>
-              eth:{" "}
+              eth:{' '}
               <input
                 value={amount}
                 onChange={(e) => {
-                  setAmount(e.target.value);
+                  setAmount(e.target.value)
                 }}
                 type="number"
                 name="amount"
@@ -178,25 +205,32 @@ export default function Home() {
               onClick={() => processBid()}
               type="submit"
               value="Submit"
-              style={{ color: hasJoined ? "grey" : "gold" }}
+              style={{ color: hasJoined ? 'grey' : 'gold' }}
             />
           </div>
           <div
             onClick={() => {
-              setTeam("right");
-              setIsTeamChosen(true);
+              setTeam('right')
+              setIsTeamChosen(true)
             }}
-            className={team === "right" ? styles.cardClicked : styles.card}
+            className={team === 'right' ? styles.cardClicked : styles.card}
           >
             <h2>Right</h2>
           </div>
         </div>
+        {hasEventFired && (
+          <div>
+            <p>Team: {bid.team}</p>
+            <p>Address: {bid.address}</p>
+            <p>id: {bid.id}</p>
+          </div>
+        )}
         {choseTeamErr && !hasJoined && (
           <div>
-            <h2 style={{ color: "crimson" }}>Please select a team</h2>
+            <h2 style={{ color: 'crimson' }}>Please select a team</h2>
           </div>
         )}
       </main>
     </div>
-  );
+  )
 }
