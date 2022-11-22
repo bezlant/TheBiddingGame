f547dffa7a6315d080cb066fc9fee993472fb31b
bezlant <abezludniy@mail.ru>
2022-11-22 15:01:40 +0300

    feat: justify-start

--- pages/index.js
+++ pages/index.js
@@ -180,7 +180,7 @@ export default function Home() {
       </Head>
 
       <main className="h-screen w-screen bg-hero-pattern bg-cover bg-center bg-no-repeat">
-        <div className="flex min-h-screen  flex-1 flex-col items-center justify-around py-4 px-4">
+        <div className="flex min-h-screen  flex-1 flex-col items-center justify-start py-4 px-4">
           <div className="flex flex-col items-center justify-center">
             <h1 className="text-4xl leading-tight">Match begins in:</h1>
             <h1>Countdown here</h1>
@@ -222,7 +222,6 @@ export default function Home() {
           </div>
           <div className="flex flex-col">
             <label>
-              eth:{' '}
               <input
                 value={amount}
                 onChange={(e) => {
@@ -231,6 +230,7 @@ export default function Home() {
                 type="number"
                 name="amount"
                 readOnly={hasJoined}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
               />
             </label>
             {showPotentialGain && <div>Potential Gain : {potentialGain}</div>}
@@ -238,7 +238,9 @@ export default function Home() {
               onClick={() => processBid()}
               type="submit"
               value="Submit"
-              className={hasJoined ? 'text-red-500' : 'text-yellow-500'}
+              className={
+                'w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto'
+              }
             />
           </div>
           {hasEventFired && (
