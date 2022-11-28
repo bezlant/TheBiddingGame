3a8136aa965c366d024335368fe7423a63fd4025
bezlant <abezludniy@mail.ru>
2022-11-28 09:59:09 +0300

    fix: Tooltip color

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -2,7 +2,7 @@ export default function Tooltip() {
   return (
     <div className="group relative top-6 right-1 z-10 flex items-end justify-end">
       <svg
-        className="h-5 w-5 text-yellow-300"
+        className="h-5 w-5 text-gray-200 hover:text-yellow-300"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
         fill="currentColor"
