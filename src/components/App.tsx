cec3e404998837c282afe483f0786bdc19483de8
bezlant <abezludniy@mail.ru>
2023-01-05 13:01:07 +0300

    feat: added flag svgs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,6 @@
 import { Web3Button } from '@web3modal/react'
+import Germany from 'public/svg/flag-icons/de.svg'
+import France from 'public/svg/flag-icons/fr.svg'
 import { FC } from 'react'
 import { useAccount } from 'wagmi'
 
@@ -8,6 +10,10 @@ const App: FC = () => {
   return (
     <div className='flex h-screen items-center justify-center bg-primary-900'>
       {isConnected ? <div>Welcome to TheEther.Bet!</div> : <Web3Button />}
+      <div className='flex w-screen justify-between'>
+        <Germany className='text-9xl' />
+        <France className='text-9xl' />
+      </div>
     </div>
   )
 }
