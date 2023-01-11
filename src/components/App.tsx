77214b34d192aed80abfd4fb20a3c6fff35b40ee
bezlant <abezludniy@mail.ru>
2023-01-11 15:01:07 +0300

    feat: added flags with the correct sizes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -22,15 +22,27 @@ const App: FC = () => {
 
   return (
     <div className='flex h-screen items-center justify-center bg-primary-900'>
-      {isConnected ? <div>Welcome to TheEther.Bet!</div> : <Web3Button />}
-      {/* This will error out if you don't connect to a wallet (TODO: Polypane metamask) */}
-      <div>Time left: {isSuccess && <span>{Number(data)}</span>}</div>
-      <div className='flex w-screen justify-between'>
-        <Germany className='text-9xl' />
-        <France className='text-9xl' />
+      {/* {isConnected ? <div>Welcome to TheEther.Bet!</div> : <Web3Button />} */}
+      {/* <div>Time left: {isSuccess && <span>{Number(data)}</span>}</div> */}
+      <div className='flex w-screen '>
+        <div className='flex w-1/3 flex-1 items-center'>
+          <Germany
+            preserveAspectRatio='none'
+            width='100%'
+            height='100%'
+            className='clip-right h-1/3 rounded-r-md transition-all duration-300 ease-linear'
+          />
+        </div>
+        <div className='flex w-1/3 flex-1 items-center justify-end'>
+          <France
+            preserveAspectRatio='none'
+            width='100%'
+            height='100%'
+            className='clip-left h-1/3 rounded-l-md transition-all duration-300 ease-linear '
+          />
+        </div>
       </div>
     </div>
   )
 }
-
 export default App
