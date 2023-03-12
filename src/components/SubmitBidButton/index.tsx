68bf03caaf3620eab65470c888f79c9321d5c643
bezlant <abezludniy@mail.ru>
2023-03-12 08:49:19 +0300

    feat: SubmitBidButton added

---
+++ src/components/SubmitBidButton/index.tsx
@@ -0,0 +1,9 @@
+const SubmitBidButton = () => {
+  return (
+    <button className="font-fugaz w-full rounded-lg bg-blue-600 py-2.5 text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-800">
+      ENTER
+    </button>
+  );
+};
+
+export default SubmitBidButton;
