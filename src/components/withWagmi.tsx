08825c60cdde94c4ae790f6ddafa4ef2a69b3df1
bezlant <abezludniy@mail.ru>
2023-03-12 08:59:26 +0300

    fix: HOC now works

--- src/components/withWagmi.tsx
+++ src/components/withWagmi.tsx
@@ -7,11 +7,10 @@ import { Web3Modal } from '@web3modal/react';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { goerli } from 'wagmi/chains';
 
-if (import.meta.env.VITE_PROJECT_ID === undefined)
-  throw new Error('Please define the PROJECT_ID');
+const projectId = import.meta.env.VITE_PROJECT_ID;
+if (projectId === undefined) throw new Error('Please define the PROJECT_ID');
 
 const chains = [goerli];
-const projectId = import.meta.env.VITE_PROJECT_ID;
 
 const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiClient = createClient({
@@ -21,14 +20,18 @@ const wagmiClient = createClient({
 });
 const ethereumClient = new EthereumClient(wagmiClient, chains);
 
-const withWagmi = (Element: React.FC) => (
-  <>
-    <WagmiConfig client={wagmiClient}>
-      <Element />
-    </WagmiConfig>
+export function withWagmi(WrappedComponent: React.FC) {
+  const ComponentWithWagmi = () => (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <WrappedComponent />
+      </WagmiConfig>
 
-    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-  </>
-);
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  );
+
+  return ComponentWithWagmi;
+}
 
 export default withWagmi;
