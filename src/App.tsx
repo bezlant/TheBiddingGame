d5c61e68ef470eb6eabeb14b18e015bd3a4b5ac8
bezlant <abezludniy@mail.ru>
2023-03-12 08:44:57 +0300

    feat: Flags

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { Web3Button } from '@web3modal/react';
 import { FC } from 'react';
 import { useAccount } from 'wagmi';
 
-import Germany from '@/assets/svg/flag-icons/de.svg';
-import France from '@/assets/svg/flag-icons/fr.svg';
-
-import BidInput from './components/BidInput';
-import Countdown from './components/Countdown';
-import withWagmi from './components/withWagmi';
+import BidInput from '@/components/BidInput';
+import Countdown from '@/components/Countdown';
+import Flags from '@/components/Flags';
+import withWagmi from '@/components/withWagmi';
 
 const App: FC = () => {
   const { isConnected } = useAccount();
@@ -17,7 +15,7 @@ const App: FC = () => {
       {isConnected ? (
         <div>
           <Countdown />
-          <Flags Left={Germany} Right={France} />
+          <Flags />
           <BidInput />
           <SubmitBidButton />
         </div>
---
+++ src/components/Flags/index.tsx
@@ -0,0 +1,25 @@
+import { ReactComponent as Germany } from '@/assets/svg/flag-icons/de.svg';
+import { ReactComponent as France } from '@/assets/svg/flag-icons/fr.svg';
+
+const Flags = () => {
+  const flagClasses =
+    'h-16 w-9/12 transition-all duration-300 ease-linear hover:w-full md:h-20';
+  return (
+    <div className="flex w-screen">
+      <div className="flex flex-1">
+        <Germany
+          preserveAspectRatio="none"
+          className={`${flagClasses} clip-right`}
+        />
+      </div>
+      <div className="flex flex-1 justify-end">
+        <France
+          preserveAspectRatio="none"
+          className={`${flagClasses} clip-left`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Flags;
