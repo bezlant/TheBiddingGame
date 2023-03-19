3e02cbcd68817d7525c82583a1a627d76c17eb17
bezlant <abezludniy@mail.ru>
2023-03-19 10:53:52 +0300

    feat: Fonts added

--- src/components/SubmitBidButton/index.tsx
+++ src/components/SubmitBidButton/index.tsx
@@ -1,6 +1,6 @@
 const SubmitBidButton = () => {
   return (
-    <button className="font-fugaz w-full rounded-lg bg-blue-600 py-2.5 text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-800">
+    <button className="w-full rounded-lg bg-blue-600 py-2.5 font-fugaz text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-800">
       ENTER
     </button>
   )
